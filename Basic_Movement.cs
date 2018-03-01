08a9e25a87fca07c28cbe1fb11ef718378fa7706
Shiri98 <erickprado309@gmail.com>
2018-02-28 20:00:02 -0500

    array update
    Added arrays and cleaned up code

--- Basic_Movement.cs
+++ Basic_Movement.cs
@@ -14,46 +14,54 @@ public class Basic_Movement : MonoBehaviour {
     public int maxEnemy = 8;
     Vector3 playerStart;
     public GameObject background;
-    public GameObject Enemy;
+    public GameObject [] Enemy;
     public int score = 0;
     public Text scoreText;
     public float enemyspeed = 10;
+    int randoX, randoZ;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         playerStart = MyObj.transform.position;
-        int randoX = (int)Random.Range(minGrid,maxGrid / gridSize);
-        Debug.Log(randoX);
-        randoX *= gridSize;
-        int randoZ = (int)Random.Range(minGrid, maxGrid / gridSize);
-        Debug.Log(randoZ);
-        randoZ *= gridSize;
+        RandoPosi();
         winspot.transform.position = new Vector3(randoX, winspot.transform.position.y, randoZ);
         scoreText.text = "score:" + score.ToString();
 
-        while (randoX == winspot.transform.position.x && randoZ == winspot.transform.position.z) {
-            randoX = (int)Random.Range(minEnemy / gridSize, maxEnemy / gridSize);
-            Debug.Log(randoX);
-            randoX *= gridSize;
-            randoZ = (int)Random.Range(minEnemy / gridSize, maxEnemy / gridSize);
-            Debug.Log(randoZ);
-            randoZ *= gridSize;
-            Enemy.transform.position = new Vector3(randoX, Enemy.transform.position.y, randoZ);
+        for (int i = 0; i < Enemy.Length; i++)
+        {
+            RandoPosi();
+            while (randoX == winspot.transform.position.x && randoZ == winspot.transform.position.z)
+            {
+                RandoPosi();
+            }
+            Enemy[i].transform.position = new Vector3(randoX, Enemy[i].transform.position.y, randoZ);
+            // winspot.transform.position = new Vector3(Random.Range(2f, 8f), 2, Random.Range(2f, 8f));
         }
-
-        Enemy.transform.position = new Vector3(randoX, Enemy.transform.position.y, randoZ);
-        // winspot.transform.position = new Vector3(Random.Range(2f, 8f), 2, Random.Range(2f, 8f));
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         PlayMovement();
         CheckBoundary();
         CheckWin();
         CheckEnemy();
         EnemyMovement();
         //Enemy.transform.position = ;
-        Enemy.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
+        for (int i = 0; i < Enemy.Length; i++)
+        {
+            Enemy[i].GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
+        }
+    }
+
+    void RandoPosi()
+    {
+        randoX = (int)Random.Range(minEnemy / gridSize, maxEnemy / gridSize);
+        randoX *= gridSize;
+        randoZ = (int)Random.Range(minEnemy / gridSize, maxEnemy / gridSize);
+        randoZ *= gridSize;
+
     }
     void PlayMovement()
     {
@@ -121,27 +129,33 @@ public class Basic_Movement : MonoBehaviour {
     }
 
     void CheckEnemy() {
-        if(MyObj.transform.position == Enemy.transform.position) {
-            MyObj.transform.position = playerStart;
-            background.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
-            Enemy.GetComponent<AudioSource>().Play();
-            score--;
-            Start();
+        for (int i = 0; i < Enemy.Length; i++){
+            if (MyObj.transform.position == Enemy[i].transform.position)
+            {
+                MyObj.transform.position = playerStart;
+                background.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
+                Enemy[i].GetComponent<AudioSource>().Play();
+                score--;
+                Start();
+            }
         }
     }
 
     void EnemyMovement()
     {
-        Enemy.transform.position += new Vector3(gridSize / enemyspeed, 0f, 0f);
-
-        if(Enemy.transform.position.x > maxEnemy)
+        for (int i = 0; i < Enemy.Length; i++)
         {
-            Enemy.transform.position = new Vector3(minEnemy, Enemy.transform.position.y, Enemy.transform.position.z - gridSize);
-        }
+            Enemy[i].transform.position += new Vector3(gridSize / enemyspeed, 0f, 0f);
 
-        if(Enemy.transform.position.z < minEnemy)
-        {
-            Enemy.transform.position = new Vector3(minEnemy, Enemy.transform.position.y, maxEnemy);
+            if (Enemy[i].transform.position.x > maxEnemy)
+            {
+                Enemy[i].transform.position = new Vector3(minEnemy, Enemy[i].transform.position.y, Enemy[i].transform.position.z - gridSize);
+            }
+
+            if (Enemy[i].transform.position.z < minEnemy)
+            {
+                Enemy[i].transform.position = new Vector3(minEnemy, Enemy[i].transform.position.y, maxEnemy);
+            }
         }
     }
 }
